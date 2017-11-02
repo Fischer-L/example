f4c167c5ca1f9e6d90a835e8a583582a1f9abba6
Fischer-L <Fischer-L@users.noreply.github.com>
2017-11-02 04:10:24 -0500

    Create test2.js

---
+++ test2.js
@@ -0,0 +1,31 @@
+  
+  
+  
+  
+  let global_variable = "I'm global";
+  function exchange() {
+    let currency = {
+      _allowed: true,
+
+      country: "GBP",
+
+      exchange: function(usd) {
+
+        let helper = {
+          rate: 1.33,
+
+          cal: function(usd) {
+            if (currency._allowed) {
+               return this.rate * usd;
+            }
+            return -1;
+          }
+        };
+
+        let gbp = helper.cal(usd);
+        gbp && console.log(`Exchanged ${usd} to ${this.country} ${gbp}`);
+      }
+    };
+    currency.exchange(100);
+  }
+  
