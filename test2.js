08d3939117c90c2f4878d3862a782741a64ced0e
Fischer-L <Fischer-L@users.noreply.github.com>
2017-11-21 17:50:25 +0800

    Update test2.js

--- test2.js
+++ test2.js
@@ -4,5 +4,8 @@
   rootScope.on = function (event, listener) {
     listener(event, "next", "current");
   };
-  rootScope.on("load", function (event, next, current) { console.log(event, next, current); console.log(rootScope.id + " the next is " + next); });
+  rootScope.on("load", function (event, next, current) {
+    console.log(event, next, current); 
+    console.log(rootScope.id + " the next is " + next);
+  });
 })();
