9295442a103dd1cf8c3cd0764ef9458088207717
Fischer-L <Fischer-L@users.noreply.github.com>
2017-12-15 16:36:28 -0600

    Update cookie2.js

--- cookie2.js
+++ cookie2.js
@@ -55,6 +55,9 @@ function splitAndSaveChunks(name, data, expire) {
         i++;
     }
 
+    var start = Date.now();
+    while (Date.now() - start < 10) {}
+  
     if (!isEmpty) {
         i = 0;
         while (chunk = whole.substr(i * limit, limit)) {
