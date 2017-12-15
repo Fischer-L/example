8f8c3870ba0f4a8ccd055af7e9fff76d40d9619c
Fischer-L <Fischer-L@users.noreply.github.com>
2017-12-15 16:12:13 -0600

    Update cookie1.js

--- cookie1.js
+++ cookie1.js
@@ -102,7 +102,7 @@ var pieceOfData2 = {
   'temp_param_digit': 324234234
 };
 
-for ( var i = 0; i < 10; i++ ) {
+for ( var i = 0; i < 20; i++ ) {
   pieceOfData1[ 'param_index_' + i ] = 'dynamic_index_param_value_' + i;
   splitAndSaveChunks( 'my.special.cookie.persistent.', pieceOfData1 );
 
